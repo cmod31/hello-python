9907d56a2924cb5fb8c574d32b39303ee9d2caa8
cmod31 <yannik@vaporvee.com>
2022-06-10 09:43:44 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,35 @@
+"""
+multiline
+comments
+1
+2
+"""
+print("hello world!")
+hello = 'hello2'
+world = 'world2'
+x = 1
+y = 2
+z = x + y
+a = hello + world
+if a == 'hello2world2':
+   print a , z
+
+mylist = [2,3,4,5]
+print('ich esse %s kinder' % mylist)
+
+number = 11.0 / 2.0 + 8.0 * 4.0
+print number
+#without comma
+numberweird = 11 / 2 + 8 * 4
+print numberweird
+
+#5hoch2
+squared = 5**2
+print squared
+cubed = 3**3
+print cubed
+
+lotsofhellow = 'hello ' *10
+print lotsofhellow
+
+print([1,2,3] * 3)
